82a47ac624904ce67463ac24cdebafd424222527
Gaurav Singh Bora <91014874+gr-12@users.noreply.github.com>
2022-10-09 20:58:12 +0530

    Update MainActivity.kt

--- app/src/main/java/com/gauravbora/getcurrentlocation/MainActivity.kt
+++ app/src/main/java/com/gauravbora/getcurrentlocation/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.gauravbora.getcurrentlocation
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.LocationListener
 import android.location.Location
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +14,7 @@ import android.provider.Settings
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 
@@ -20,6 +23,10 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private lateinit var tvLongitude: TextView
+    private val REQUEST_CODE = 1000
+    var locationManager: LocationManager? = null
+    var locationListener: LocationListener? = null
+
     private lateinit var tvLatitude: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,124 +38,102 @@ class MainActivity : AppCompatActivity() {
         tvLatitude = findViewById(R.id.latitude)
         tvLongitude = findViewById(R.id.longitude)
 
-        getCurrentLocation()
 
+        locationManager = this.getSystemService(LOCATION_SERVICE) as LocationManager
+        locationListener = object : LocationListener {
+            @SuppressLint("SetTextI18n")
+            override fun onLocationChanged(location: Location) {
 
-    }
-
-
-
-
-
-    @SuppressLint("SetTextI18n")
-    private fun getCurrentLocation() {
-
-        if (checkPermission()) {
-            if (isLocationEnabled()) {
-
-//                here we will get the final latitude and longitude
-
-                fusedLocationProviderClient.lastLocation.addOnCompleteListener(this) { task ->
-
-                    val location: Location? = task.result
-                    if(location==null){
-                        Toast.makeText(this,"NUll",Toast.LENGTH_SHORT).show()
-
-                    }
-                    else{
-                        Toast.makeText(this,"Get Location Success",Toast.LENGTH_SHORT).show()
-                        tvLatitude.text= "Latitude is : "+location.latitude
-                        tvLongitude.text="Longitude is : "+location.longitude
-
-                    }
-
+                if (location == null) {
+                    Toast.makeText(applicationContext, "NUll", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(applicationContext, "Get Location Success", Toast.LENGTH_SHORT)
+                        .show()
+                    tvLatitude.text = "Latitude: ${location.latitude}"
+                    tvLongitude.text = "Latitude: ${location.longitude}"
                 }
+            }
 
+            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+                super.onStatusChanged(provider, status, extras)
+            }
 
-            } else {
-//                setting open
-
-                Toast.makeText(this,"Turn on Location",Toast.LENGTH_SHORT).show()
-                val intent= Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
-                startActivity(intent)
+            override fun onProviderDisabled(provider: String) {
 
             }
-        } else {
-//request permission
-            requestPermission()
+
+            override fun onProviderEnabled(provider: String) {
+
+            }
+
         }
 
-
-    }
-
-
-    private fun isLocationEnabled():Boolean{
-        val locationManager:LocationManager= getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        return  locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-                ||
-                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-
-
-    }
-
-
-
-
-    private fun requestPermission() {
-
-        ActivityCompat.requestPermissions(
-            this, arrayOf(
-                android.Manifest.permission.ACCESS_FINE_LOCATION,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION
-            ),
-            PERMISSION_REQUEST_LOCATION
-        )
-
-    }
-
-    companion object {
-        private const val PERMISSION_REQUEST_LOCATION = 100
-    }
-
-
-    private fun checkPermission(): Boolean {
-
-        if (ActivityCompat.checkSelfPermission(
+        if (ContextCompat.checkSelfPermission(
                 this,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION
-            )
-            == PackageManager.PERMISSION_GRANTED
-            &&
-            ActivityCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) !=
+            PackageManager.PERMISSION_GRANTED
         ) {
-            return true
+            Toast.makeText(this, "Denied", Toast.LENGTH_SHORT).show()
+
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ), REQUEST_CODE
+            )
+        } else {
+
+            if (isLocationEnabled()) {
+
+                locationManager!!.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER,
+                    0, 10f,
+                    locationListener as LocationListener
+                )
+            } else {
+                Toast.makeText(this, "Turn on Location", Toast.LENGTH_SHORT).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
         }
-        return false
+
+
     }
 
-//    for user location-> allow or deny
-//    handling permission result    ->   inbuild
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-    if(requestCode== PERMISSION_REQUEST_LOCATION) {
-        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-        {
-            Toast.makeText(applicationContext,"Granted",Toast.LENGTH_SHORT).show()
-            getCurrentLocation()
-        }
-        else{
-            Toast.makeText(applicationContext,"Denied",Toast.LENGTH_SHORT).show()
+        if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                locationManager!!.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER,
+                    0, 0f,
+                    locationListener as LocationListener
+                )
+            }
         }
     }
 
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+
+
     }
+
 
 }
